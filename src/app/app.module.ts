8dcbc7a438f54c771259d6089cc094e040c386e4
Ming Hao Chan <cmh114933@gmail.com>
2019-04-19 12:08:02 +0800

    boilerplate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToDoListComponent } from './to-do-list/to-do-list.component';
+import { ToDoFormComponent } from './to-do-form/to-do-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToDoListComponent,
+    ToDoFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/to-do-form/to-do-form.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-to-do-form',
+  templateUrl: './to-do-form.component.html',
+  styleUrls: ['./to-do-form.component.css']
+})
+export class ToDoFormComponent implements OnInit {
+  // V1
+  // - Create a form field to add new todo items todo list (taskName)
+  // - Display preview before adding to list (Accessing value : fieldName.value)
+  // - Clear input (Programmatically setting value: fieldName.setValue())
+  // - Make todo item form field Required and minLength (Adding Validators.required)
+  // - If field invalid, disabled addButton (Accessing form input status: fieldName.status/ fieldName.invalid && (fieldName.dirty || fieldName.touched)")
+  // - Display error message (Get list of errors : fieldName.errors)
+
+  // V2
+  // - Expand todolist input to receive multiple values ( taskName, taskDescription, assignedTo, difficultyRating)
+  // - Create more input fields 
+  // - Preview area to display values
+  // - Submit Form on Enter/<input type submit> ( ngSubmit to capture value of current form )
+  // - Disabled submit if any point of form group is invalid 
+  // - New Approach: Trigger validation only on submit (boolean val to set displayError)
+
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
